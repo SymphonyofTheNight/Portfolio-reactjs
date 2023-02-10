13adad3fcebead3134cd201295c8a0bfa603de6d
gino <gamexgaming1997@gmail.com>
2023-02-10 14:37:07 +0800

    just save

---
+++ client/src/assets/vector/vector.js
@@ -0,0 +1,23 @@
+import responsive from '../vector/responsive.svg';
+import programmer from '../vector/programmer.svg';
+import collaboration from '../vector/collaboration.svg';
+
+const vector = [
+    {
+        label: 'Responsive Website',
+        svg: responsive,
+        description: 'Develop a responsive website layout for a better user experience.'
+    },
+    {
+        label: 'Convert your design into a working website.',
+        svg: programmer,
+        description: 'Make an ideal functional website with your mockups and UI prototypes.'
+    },
+    {
+        label: 'Team Collaboration',
+        svg: collaboration,
+        description: 'Can work as part of a team and has outstanding collaboration skills.'
+    },
+]
+
+export default vector;
--- client/src/components/Landingpage/Landingpage.jsx
+++ client/src/components/Landingpage/Landingpage.jsx
@@ -7,9 +7,12 @@ import developer from '../../developer/developer';
 // techstack 
 import techstack from '../../developer/techstack';
 
-import gsap from 'gsap';
-import ScrollTrigger from 'gsap/ScrollTrigger';
-gsap.registerPlugin(ScrollTrigger);
+// vector
+import vector from '../../assets/vector/vector';
+
+// import gsap from 'gsap';
+// import ScrollTrigger from 'gsap/ScrollTrigger';
+// gsap.registerPlugin(ScrollTrigger);
 
 const Landingpage = ({ colortoggle, setColortoggle }) => {
 
@@ -41,16 +44,47 @@ const Landingpage = ({ colortoggle, setColortoggle }) => {
 
             </div>
 
+            {/* Services */}
+            <div className={`w-[full] h-[90vh] mt-[20vh] relative grid items-center justify-center services`}>
+                <span className={`font-Oxygen font-bold text-[.7em] ${!colortoggle ? 'text-white' : 'text-black'} tracking-[5px] absolute label`}>
+                    SERVICES
+                </span>
+                <div className={`h-[90vh] w-[70vw] relative flex flex-wrap flex-row items-center justify-around`}>
+                    <span className={`text-[2.5em] font-Oxygen absolute text-white top-0 mt-[2vh]`}>
+                        What I <span className={`text-yellow-500`}>offer?</span>
+                    </span>
+                    {Object.keys(vector).map(state => {
+                        return <div key={vector[state].label} className={`h-[38vh] w-[19vw] flex flex-wrap items-top justify-center text`}>
+                            <img className='h-[18vh] mt-[5vh]' src={vector[state].svg} alt={vector[state].label} />
+                            <h1 className={`font-Oxygen ${!colortoggle ? 'text-yellow-500' : 'text-black'} font-bold text-[1.1em]`}>
+                                {vector[state].label}
+                            </h1>
+                            <span className={`font-Oxygen ${!colortoggle ? 'text-white' : 'text-black'} text-center`}>
+                                {vector[state].description}
+                            </span>
+                        </div>
+                    })}
+                </div>
+            </div>
+
             {/* Technologies using */}
-            <div className='h-[70vh] w-[100vw] mt-[30vh] relative grid items-center justify-center technologies'>
+            <div className='h-[70vh] w-[100vw] mt-[20vh] relative grid items-center justify-center technologies'>
                 <span className={`font-Oxygen font-bold text-[.7em] ${!colortoggle ? 'text-white' : 'text-black'} tracking-[5px] absolute label`}>
                     TECHNOLOGIES
                 </span>
+                <span className={`font-Oxygen font-bold text-[2em] ${!colortoggle ? 'text-white' : 'text-black'} tracking-[5px]`}>
+                    My <span className={`text-yellow-500`}>Skills</span>
+                </span>
                 <div type='input' className='h-[auto] w-[60vw] rounded-[20px] relative flex flex-wrap items-center justify-center'>
                     {Object.keys(techstack).map(tech => {
-                        return <img className='h-[10vh] m-5 grayscale hover:grayscale-0 trasition duration-[.3s]' src={techstack[tech].svg} alt={techstack[tech].tech} key={techstack[tech].tech} />
+                        return <img className='h-[9.5vh] m-5 grayscale hover:grayscale-0 trasition duration-[.3s]' src={techstack[tech].svg} alt={techstack[tech].tech} key={techstack[tech].tech} />
                     })}
                 </div>
+            </div>
+
+            {/* projects */}
+            <div className='h-[200vh] w-full bg-green-700 mt-[30vh]'>
+
             </div>
 
         </div>
